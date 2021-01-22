9d8840a6f50ebd39018f19e9f9007092e81f55e2
Loren Cipriano <loren25cipriano@gmail.com>
2021-01-21 21:27:36 -0800

    commit tree 9c9bb5f8becda74c0957df00efd1547916625bb2

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,14 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+
+-- schema_migration.sql
+--   creates pokemon table
+--   has an autoincrementing id column
+--   has a name column
+--   has a type column
+
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    type TEXT
+);
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db 
+
+    def initialize(id:, name:, type:, db:)
+        @id = id 
+        @name = name 
+        @type = type 
+        @db = db
+    end
+
+    # .save
+    # saves an instance of a pokemon with the correct id
+
+    def self.save(name, type, db) 
+    # Pokemon.save("Pikachu", "electric", @db)
+
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
+        # expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+
+    end 
+
+    # Pokemon.save("Pikachu", "electric", @db)
+
+    # pikachu_from_db = Pokemon.find(1, @db)
+    # expect(pikachu_from_db.id).to eq(1)
+    # expect(pikachu_from_db.name).to eq("Pikachu")
+    # expect(pikachu_from_db.type).to eq("electric")
+
+    def self.find(id_num, db) 
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+        Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    end 
+
 end
